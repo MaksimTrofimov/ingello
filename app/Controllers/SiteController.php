31006b35d35233c151a2e4846e8fabeed63eb136
Maksim Trofimov <maksim.t.kh@gmail.com>
2020-03-01 12:41:19 +0200

    autoload, siteController

---
+++ app/Controllers/SiteController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controllers;
+
+class SiteController
+{
+    public function index()
+    {
+        echo __METHOD__;
+    }
+
+    public function contact()
+    {
+        echo __METHOD__;
+    }
+}
