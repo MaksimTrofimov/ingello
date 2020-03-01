72dbd97f1f3dccc4f016161b10f55ad78efe99a7
Maksim Trofimov <maksim.t.kh@gmail.com>
2020-03-01 13:46:06 +0200

    Added methods to module classes

---
+++ app/Modules/Store/Controllers/ProductController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Modules\Store\Controllers;
+
+class ProductController
+{
+    public function index()
+    {
+        echo __METHOD__;
+    }
+
+    public function create()
+    {
+        echo __METHOD__;
+    }
+}
---
+++ app/Modules/User/Controllers/AuthController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Modules\User\Controllers;
+
+class AuthController
+{
+    public function createToken()
+    {
+        echo __METHOD__;
+    }
+}
